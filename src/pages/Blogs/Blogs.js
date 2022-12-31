2ab449020719b60cdf149e8355aa7d4f354fe472
joydharsamrat <joydharsamrat@gmail.com>
2022-12-31 19:27:06 +0600

    blog page updated

--- src/pages/Blogs/Blogs.js
+++ src/pages/Blogs/Blogs.js
@@ -3,7 +3,9 @@ import { Helmet } from 'react-helmet-async';
 
 const Blogs = () => {
     return (
-        <div></div>
+        <div>
+            <img className='h-screen w-screen' src="https://i.ibb.co/17dzVC6/27282.jpg" alt="" />
+        </div>
     );
 };
 
